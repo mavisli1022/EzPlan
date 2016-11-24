a8f91f29152cae7dfbb924e6481c7165a37a4226
SHREY-PC\Shreyansh <shrey@rushhourmedia.hk>
2016-11-24 01:32:41 -0500

    simplified server.js

--- upload.js
+++ upload.js
@@ -11,10 +11,29 @@ var app = express();
 app.use(express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/views'));
 
+
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
+
+var upload = multer({dest: './upload/'});
+
+
+
+
+app.post('/upload', upload.single('calendar_user'), function(req, res, next){
+    //console.log("here");
+
+    //TODO: handle the user name
+    var a = routes.convertCal('./upload/coursesCalendar.ics');
+
+    var b = routes.processCourse(a);
+    res.send(b);
+});
+
+
 
 app.listen(3000);
 console.log('Listening on port 3000');
