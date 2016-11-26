4799c0384f445eefb024187c27ecc350a48d6c4e
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2016-11-26 01:19:34 -0500

    finished front end of login screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,38 @@ $(function(){
 
     }, {scope: 'public_profile,email'});
   })
+
+  $("#login-link").click(function(e){
+    e.preventDefault();
+    if($("#signup-link").hasClass("active")){
+      $("#signup-link").removeClass("active");
+      $("#login-link").addClass("active");
+      //+150
+      $("#login-nav .selected").animate({
+        left: "-=150"
+      }, 1000, function(){
+        //if this is not active, hide this and show other one
+        $(".sign-up-box").fadeOut("slow", function(){
+          $(".login-box").fadeIn("slow");
+        });
+      });
+    }
+  })
+
+  $("#signup-link").click(function(e){
+    e.preventDefault();
+    if($("#login-link").hasClass("active")){
+      $("#login-link").removeClass("active");
+      $("#signup-link").addClass("active");
+
+      $("#login-nav .selected").animate({
+        left: "+=150"
+      }, 1000, function(){
+        //if this is not active, hide this and show other one
+        $(".login-box").fadeOut("slow", function(){
+          $(".sign-up-box").fadeIn("slow");
+        });
+      });
+    }
+  })
 })
