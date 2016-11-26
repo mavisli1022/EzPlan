16072d85c75c2cf5c3257bde8d470987c5ed9f26
Yang Cao <yang.cao@mail.utoronto.ca>
2016-11-26 03:03:21 -0500

    simple comparison

--- routes.js
+++ routes.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var util = require("util");
+var fs = require("fs");
+
+    var ttObj;
+    fs.readFile('jsonfile.JSON', 'utf-8', function(err, data) {
+    if(err) throw err;
+    ttObj = JSON.parse(data);
+    });
 
 //read file and get the coursecode as well as section from calendar
 exports.convertCal= function(filename){
@@ -19,12 +26,46 @@ exports.convertCal= function(filename){
 }
 
 //process data as json
-exports.processCourse= function(courseSummary){
+exports.processCourse= function(courseSummary, name){
     console.log(courseSummary)
     var personCal = JSON.parse('{}');
-    personCal['name'] = "--";
+    personCal['name'] = name;
     personCal['courseSummary'] = courseSummary;
     return personCal
+}
+
+
+exports.compare= function(req, res){
+    var name1 = '1';
+    var name2 = '2';
+
+    var a;
+    var b;    
+
+    //console.log("into"+ JSON.stringify(ttObj));
+    for(var i=0; i<ttObj.length;i++){
+        if(ttObj[i].name == name1)
+            a = ttObj[i];
+        else if(ttObj[i].name == name2)
+            b= ttObj[i];
+    }
+
+    var returnOBJ=[];
+    var count=0;
+
+    for(var j=0; j<a.courseSummary.length;j++){
+        for(var m=0; m<b.courseSummary.length;m++){
+            if(a.courseSummary[j] == b.courseSummary[m])
+                {
+                returnOBJ[count]=a.courseSummary[j];
+                count++;
+                }
+
+            }
+        }
+
+        res.send(returnOBJ);
+
 }
 
 
--- server.js
+++ server.js
@@ -1,17 +1,48 @@
+var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
-var FB = require('fb');
+var multer = require('multer');
+
+var routes = require('./routes.js');
+
 
 var app = express();
-
-//assets and files
-app.use(express.static(__dirname + '/assets'));
-app.use(express.static(__dirname + '/views'));
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
-app.listen(process.env.PORT || 3000);
+var upload = multer({dest: './upload/'});
+
+app.get('/', function(req, res) {
+    res.sendfile('./comparison.html');
+});
+
+
+app.post('/upload', upload.single('calendar_user'), function(req, res, next){
+  var a = routes.convertCal('./upload/coursesCalendar.ics');
+    var c =  routes.convertCal('./upload/courses_Calendar.ics');
+
+    var array = [];
+
+
+    var b = routes.processCourse(a,'1');
+    var d = routes.processCourse(c,'2');
+    array.push(b);
+    array.push(d);
+    fs.writeFile('jsonfile.JSON', JSON.stringify(array), function (err) {
+    if (err) 
+        return console.log(err);
+    
+    });
+
+	res.send(array); 
+});
+
+app.post('/compare', routes.compare)
+
+
+
+app.listen(3000);
 console.log('Listening on port 3000');
